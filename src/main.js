bb06180fe21c20001b12161f08cb27df185e506b
ElPnkiePie <silvatapiajuanpablo@gmail.com>
2023-07-10 23:36:47 -0600

    Numpad Funcional
    Se realizaron los códigos para volver funcional el Numpad y el punto.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,75 @@
 const buttonPercent = document.getElementById("button-percent");
+const buttonSeven = document.getElementById("button-seven")
+const buttonEight = document.getElementById("button-eight")
+const buttonNine = document.getElementById("button-nine")
+const buttonFour = document.getElementById("button-four")
+const buttonFive = document.getElementById("button-five")
+const buttonSix = document.getElementById("button-six")
+const buttonOne = document.getElementById("button-one")
+const buttonTwo = document.getElementById("button-two")
+const buttonThree = document.getElementById("button-three")
+const buttonZero = document.getElementById("button-zero")
+const buttonDot = document.getElementById("button-dot")
 
 buttonPercent.addEventListener(
     "click"
     ,() => addTextSecondLine("%")
 );
+
+buttonSeven.addEventListener(
+    "click"
+    ,() => addTextSecondLine("7")
+);
+
+buttonEight.addEventListener(
+    "click"
+    ,() => addTextSecondLine("8")
+);
+
+buttonNine.addEventListener(
+    "click"
+    ,() => addTextSecondLine("9")
+);
+
+buttonFour.addEventListener(
+    "click"
+    ,() => addTextSecondLine("4")
+);
+
+buttonFive.addEventListener(
+    "click"
+    ,() =>addTextSecondLine("5")
+);
+
+buttonSix.addEventListener(
+    "click"
+    ,() => addTextSecondLine("6")
+);
+
+buttonOne.addEventListener(
+    "click"
+    ,() => addTextSecondLine("1")
+);
+
+buttonTwo.addEventListener(
+    "click"
+    ,() => addTextSecondLine("2")
+);
+
+buttonThree.addEventListener(
+    "click"
+    ,() => addTextSecondLine("3")
+);
+
+buttonZero.addEventListener(
+    "click"
+    ,() => addTextSecondLine("0")
+);
+
+buttonDot.addEventListener(
+    "click"
+    ,() => addTextSecondLine(".")
+)
 
 const addTextSecondLine = (word) => {
     const secondLine = document.getElementById("second-line");
